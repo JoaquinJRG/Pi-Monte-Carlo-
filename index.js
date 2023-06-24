d985e6cab2c4673c58a706f4eb8488122c3106ae
Joaquín <joaquinjrg01@gmail.com>
2023-06-25 00:01:33 +0200

    Añado representación de puntos generados aleatoriamente

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ rangoPuntos.addEventListener("input", function() {
 
 const simular = document.getElementById("simular");
 const resultadoPi = document.getElementById("resultadoPi");  
+const totalPuntosSalida = document.getElementById("totalPuntos"); 
+const puntosDentroSalida = document.getElementById("puntosDentro"); 
 
 simular.addEventListener("click", function(){
+
+    borrarPuntos(); 
 
     let totalPuntos = Number(numPuntos.textContent); 
     let puntosDentro = 0;
@@ -20,15 +24,22 @@ simular.addEventListener("click", function(){
     for(let i = 1; i <= totalPuntos; i++){
 
         let punto = generarPunto(); 
+        
+        dibujarPunto(punto)
 
         if(punto[0]*punto[0] + punto[1]*punto[1] <= 1){
             puntosDentro++; 
         }
+
     }
 
+    //Calculo de PI
     aproxPi = 4*(puntosDentro/totalPuntos); 
-    resultadoPi.textContent = aproxPi; 
 
+    //Mostramos los resultados
+    resultadoPi.textContent = aproxPi; 
+    totalPuntosSalida.textContent = totalPuntos; 
+    puntosDentroSalida.textContent = puntosDentro; 
 
 }); 
 
@@ -44,3 +55,50 @@ function generarPunto(){
     
     return [x,y]; 
 }
+
+//Canvas 
+let canvas = document.getElementById("grafico");
+let ctx = canvas.getContext("2d");
+
+//Dibujar Circulo
+let centerX = 0
+let centerY = 500;
+var radius = 500;
+var startAngle = 1.5 * Math.PI;
+var endAngle = Math.PI;
+
+ctx.beginPath(); 
+ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+ctx.stroke();
+
+// Dibujar puntos
+
+function dibujarPunto(punto){
+
+    let x = Math.floor(punto[0] * 500);
+    let y = Math.floor(punto[1] * 500);
+    
+    ctx.beginPath();
+    ctx.arc(x, y, 1.5, 0, 2 * Math.PI);
+
+    if((x)*(x) + (y - 500)*(y - 500) <= 500*500){
+        ctx.fillStyle = "red";
+    }else{
+        ctx.fillStyle = "blue";
+    }
+
+    ctx.fill();
+    ctx.stroke(); 
+
+}
+
+//Eliminar Puntos 
+function borrarPuntos(){
+    ctx.clearRect(0,0,500,500); 
+    ctx.beginPath(); 
+    ctx.arc(centerX, centerY, radius, startAngle, endAngle);
+    ctx.stroke();
+}
+
+
+
